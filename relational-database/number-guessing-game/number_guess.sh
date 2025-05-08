89c47c24274a9e6a07c15fabf8891937b016599b
Michael S. Wang <122252833+michaelwsd@users.noreply.github.com>
2025-05-08 12:35:24 +1000

    Add files via upload

--- relational-database/number-guessing-game/number_guess.sh
+++ relational-database/number-guessing-game/number_guess.sh
@@ -1,1 +1,46 @@
+#! /bin/bash
 
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+echo -e "\n~~ Welcome to Number Guessing Game ~~\n"
+echo "Enter your username:"
+read USERNAME
+
+ROUNDS=$($PSQL "select rounds_played from users where name = '$USERNAME'")
+echo $ROUNDS
+if [[ -z $ROUNDS ]]
+then
+  # create new user
+  INSERT_USER=$($PSQL "insert into users (name) values ('$USERNAME')")
+  echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
+else
+  SCORE=$($PSQL "select best_score from users where name = '$USERNAME'")
+  echo -e "\nWelcome back, $USERNAME! You have played $ROUNDS games, and your best game took $SCORE guesses."
+fi
+
+NUMBER_GUESS=$(($RANDOM % 1000))
+echo $NUMBER_GUESS
+
+echo -e "\nGuess the secret number between 1 and 1000:"
+read GUESS
+NUM_TRY=0
+
+while [[ $GUESS != $NUMBER_GUESS ]]
+do
+  if [[ ! $GUESS =~ ^[0-9]+$ ]]
+  then 
+    echo "That is not an integer, guess again:"
+  elif [[ $GUESS -lt $NUMBER_GUESS ]]
+  then 
+    echo "It's higher than that, guess again:"
+  else
+    echo "It's lower than that, guess again:"
+  fi
+
+  NUM_TRY=$(($NUM_TRY + 1))
+  read GUESS
+done 
+
+NUM_TRY=$(($NUM_TRY + 1))
+echo -e "\nYou guessed it in $NUM_TRY tries. The secret number was $NUMBER_GUESS. Nice job!"
+UPDATE_ROUND_RES=$($PSQL "update users set rounds_played = rounds_played + 1 where name = '$USERNAME'")
+UPDATE_SCORE_RES=$($PSQL "update users set best_score = case when best_score is not null then least(best_score, $NUM_TRY) else $NUM_TRY end where name = '$USERNAME'")
---
+++ relational-database/number-guessing-game/number_guess.sql
@@ -0,0 +1,110 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.17 (Ubuntu 12.17-1.pgdg22.04+1)
+-- Dumped by pg_dump version 12.17 (Ubuntu 12.17-1.pgdg22.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE number_guess;
+--
+-- Name: number_guess; Type: DATABASE; Schema: -; Owner: freecodecamp
+--
+
+CREATE DATABASE number_guess WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
+
+
+ALTER DATABASE number_guess OWNER TO freecodecamp;
+
+\connect number_guess
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.users (
+    user_id integer NOT NULL,
+    name character varying(22) NOT NULL,
+    rounds_played integer DEFAULT 0,
+    best_score integer
+);
+
+
+ALTER TABLE public.users OWNER TO freecodecamp;
+
+--
+-- Name: users_user_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
+--
+
+CREATE SEQUENCE public.users_user_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.users_user_id_seq OWNER TO freecodecamp;
+
+--
+-- Name: users_user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
+--
+
+ALTER SEQUENCE public.users_user_id_seq OWNED BY public.users.user_id;
+
+
+--
+-- Name: users user_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN user_id SET DEFAULT nextval('public.users_user_id_seq'::regclass);
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+INSERT INTO public.users VALUES (33, 'mic', 1, 3);
+INSERT INTO public.users VALUES (35, 'user_1746671553367', 2, 144);
+INSERT INTO public.users VALUES (34, 'user_1746671553368', 5, 44);
+INSERT INTO public.users VALUES (37, 'user_1746671599137', 2, 679);
+INSERT INTO public.users VALUES (36, 'user_1746671599138', 5, 38);
+
+
+--
+-- Name: users_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
+--
+
+SELECT pg_catalog.setval('public.users_user_id_seq', 37, true);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
